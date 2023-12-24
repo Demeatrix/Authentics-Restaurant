7ea4d143d19ef5de4e098bd562030e337d32b8e3
Shubh Khoda <shubhkhoda19@gmail.com>
2023-12-24 23:35:43 +0530

    Added Contact page and addition on home page

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ window.addEventListener("load", function() {
     preload.classList.add("loaded");
     document.body.classList.add("loaded");
 });
+
+
+let navbar = document.querySelector(".header .navbar");
+let menuBtn = document.querySelector("#menu-btn");
+
+menuBtn.onclick = () => {
+  menuBtn.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+document.getElementById('menu-btn').addEventListener('click', function() {
+  var navbar = document.querySelector('.header .navbar');
+  navbar.classList.toggle('active');
+});
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+   loop: true,
+  });
